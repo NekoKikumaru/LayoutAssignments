57a9889e250567de5c2d7b2f423e80862fa28c5d
ThuThu-PTZKG <pwintthinzarkogyi@gmail.com>
2020-03-04 03:39:49 +0630

    Two Numbers Calculator

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,40 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.test_one_layout)
+        setContentView(R.layout.activity_main)
+
+        addButton.setOnClickListener {
+            var num1: Int = numOne.text.toString().toInt()
+            var num2: Int = numTwo.text.toString().toInt()
+            var addition = num1+num2
+            result.text = addition.toString()
+        }
+
+        subButton.setOnClickListener {
+            var num1: Int = numOne.text.toString().toInt()
+            var num2: Int = numTwo.text.toString().toInt()
+            var subtraction = num1-num2
+            result.text = subtraction.toString()
+        }
+
+        mulButton.setOnClickListener {
+            var num1: Int = numOne.text.toString().toInt()
+            var num2: Int = numTwo.text.toString().toInt()
+            var multiplication = num1*num2
+            result.text = multiplication.toString()
+        }
+
+        divButton.setOnClickListener {
+            var num1: Int = numOne.text.toString().toInt()
+            var num2: Int = numTwo.text.toString().toInt()
+            var division = num1/num2
+            result.text = division.toString()
+        }
     }
 }
